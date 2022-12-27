d6be00bfabd209ac33ea56e09aae981ca5a0022c
cowboyliao <2730647052@qq.com>
2022-12-28 00:21:16 +0800

    first

---
+++ init.lua
@@ -0,0 +1,9 @@
+require('basic')
+require('colorscheme')
+require('keybindings')
+require('plugins')
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+
---
+++ lua/keybindings.lua
@@ -0,0 +1,60 @@
+local map = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+-- 复用 opt 参数
+local opt = {noremap = true, silent = true }
+map("n","<leader><CR>",":so ~/.config/nvim/init.lua<CR>",opt)
+
+
+map("n","j","e",opt)
+map("v","e","j",opt)
+map("n","e","j",opt)
+map("v","j","e",opt)
+
+
+map("v","<leader>y","\"+y",opt)
+map("n","<leader>y","\"+y",opt)
+map("n","<leader>Y","gg\"+yG",opt)
+
+
+map("v","E",":m '>+1<CR>gv=gv",opt)
+map("v","K",":m '<-2<CR>gv=gv",opt)
+
+-- bufferline
+-- 左右Tab切换
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+-- 关闭
+--"moll/vim-bbye"
+map("n", "<C-w>", ":Bdelete!<CR>", opt)
+map("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
+map("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt)
+map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
+
+-- 插件快捷键
+local pluginKeys = {}
+
+-- nvim-tree
+-- alt + m 键打开关闭tree
+map("n", "<A-m>", ":NvimTreeToggle<CR>", opt)
+-- 列表快捷键
+pluginKeys.nvimTreeList = {
+  -- 打开文件或文件夹
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
+  -- 分屏打开文件
+  { key = "v", action = "vsplit" },
+  { key = "h", action = "split" },
+  -- 显示隐藏文件
+  { key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
+  { key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
+  -- 文件操作
+  { key = "<F5>", action = "refresh" },
+  { key = "a", action = "create" },
+  { key = "d", action = "remove" },
+  { key = "r", action = "rename" },
+  { key = "x", action = "cut" },
+  { key = "c", action = "copy" },
+  { key = "p", action = "paste" },
+  { key = "s", action = "system_open" },
+}
+return pluginKeys
