eb436a061d4ae61306d9c0b8dfdbf6666cd762f1
cowboyliao <2730647052@qq.com>
2023-01-21 21:07:15 +0800

    23/1/21

--- init.lua
+++ init.lua
@@ -1,16 +1,17 @@
 require('basic')
 require('cursor')
-require('colorscheme')
+--require('colorscheme')
 require('keybindings')
-require('plugins')
+--require('plugins')
 -- 插件配置
-require("plugin-config.nvim-tree")
-require("plugin-config.bufferline")
-require("plugin-config.lualine")
-require("plugin-config.telescope")
-require("plugin-config.nvim-treesitter")
+--require("plugin-config.nvim-tree")
+--require("plugin-config.bufferline")
+--require("plugin-config.lualine")
+--require("plugin-config.telescope")
+--require("plugin-config.nvim-treesitter")
 --require("lsp.setup")
-require("plugin-config.dashboard")
-require('leap').add_default_mappings()
-require('leap').opts.safe_labels = { "a", "o", "r", "i", "s", "e",
- "t", "n","v", "k", "c", "m", "f", "u","w", "y", "b" }
+--require("plugin-config.dashboard")
+--require('leap').add_default_mappings()
+--require('leap').opts.safe_labels = { "a", "o", "r", "i", "s", "e",
+ --"t", "n","v", "k", "c", "m", "f", "u","w", "y", "b" }
+
